8a802a9f780b05dcb99870058488151688370f1d
Brice <bgatelet@gmail.com>
2015-02-20 12:57:00 +0530

    Improved email validation.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,7 +31,8 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid emails" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
@@ -39,7 +40,8 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid emails" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address} should be invalid"
