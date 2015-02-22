116abc06003f84e5c45749b4c9d229ec74ee4b0a
Brice <bgatelet@gmail.com>
2015-02-22 19:57:57 +0530

    Finished user signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Flash issues message (success, info, warning, danger) that exist only on first reload or redirect.
+      flash[:success] = "Welcome to the sample app!"
+      # Equivalent to redirect_to user_url(@user)
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    # Make sure page loads
+    get signup_path
+
+    # Checks if invalid users are saved or not
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+
+    # Make sure that signup page reloads after invalid submit
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+
+    # Checks if there is a difference of 1 (second argument) in User table.
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
+    end
+
+    assert_template 'users/show'
+  end
+end
